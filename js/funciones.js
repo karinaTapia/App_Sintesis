b73ceb4449d6a37343dc5205e359332fcb3c3fb7
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-04-25 11:03:19 -0500

    inicio

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+
+//Obener Variables GET
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+//Obener Variables GET
